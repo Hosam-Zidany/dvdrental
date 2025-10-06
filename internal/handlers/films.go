60af3e7a7e006f6efeb9340607c9d59655fcaff4
Hosam Zidany <hosam.zidany.1@gmail.com>
2025-10-06 07:18:06 +0300

    add filters to "/films"

--- internal/handlers/films.go
+++ internal/handlers/films.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/Hosam-Zidany/dvdrental/internal/models"
 
@@ -11,12 +12,36 @@ import (
 
 func GetFilms(c *gin.Context) {
 	var films []models.Film
-	result := database.DB.Find(&films)
+	page, _ := strconv.Atoi((c.DefaultQuery("page", "1")))
+	limit, _ := strconv.Atoi((c.DefaultQuery("limit", "10")))
+	offset := (page - 1) * limit
+
+	query := database.DB.Model(&models.Film{})
+
+	category := c.Query("category")
+	language := c.Query("language")
+
+	if language != "" {
+		query.Joins("JOIN language l ON l.language_id = film.language_id").
+			Where("l.name = ?", language)
+	}
+	if category != "" {
+		query.Joins("JOIN film_category fc ON fc.film_id = film.film_id").
+			Joins("JOIN category c ON c.category_id = fc.category_id").
+			Where("c.name = ?", category)
+	}
+	// result := database.DB.Find(&films)
+	result := query.Offset(offset).Limit(limit).Find(&films)
 	if result.Error != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error.Error()})
 		return
 	}
-	c.JSON(http.StatusOK, films)
+	c.JSON(http.StatusOK, gin.H{
+		"page":  page,
+		"limit": limit,
+		"count": result.RowsAffected,
+		"data":  films,
+	})
 }
 
 func GetFilmByID(c *gin.Context) {
